5aa140cdf8f78c5772b43ccd5c954fbd11e47c64
Leonid Sorkin <lsorkini@gmail.com>
2020-05-12 22:01:09 +0300

    Added active class to buttons

--- src/components/WorkTabs.jsx
+++ src/components/WorkTabs.jsx
@@ -11,11 +11,11 @@ const WorkTabs = () => {
   return (
     <div>
       <div className="button_elements">
-        <button className="btn_one" onClick={clickHandlerWork}>
-          Work
-        </button>
-        <button className="btn_one" onClick={clickHandlerProjects}>
+        <button className={`btn_one ${showProjects === false?"btn_active":null}`} onClick={clickHandlerWork}>
           Projects
+        </button>
+        <button className={`btn_one ${showProjects === true?"btn_active":null}`} onClick={clickHandlerProjects}>
+          Work
         </button>
       </div>
       <div
@@ -60,17 +60,11 @@ const WorkTabs = () => {
         }`}
         id="workId"
       >
-        <h1>works.</h1>
+        <h1>Projects</h1>
         <section>
           <h2>work title</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Projects
           </p>
           <div id="used">
             <div>
